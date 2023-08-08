84bf36fa9aa48917f34c8a5f5a80425e279b027a
TBaranchik <bar-ti@mail.ru>
2023-08-08 09:40:44 +0300

    ДЗ 3, часть2 (Ex2)

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -19,7 +24,7 @@ public class FirstTest {
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "C:\\Projects\\mobileAuto\\apks\\Wikipedia_2.7.50437-r-2023-04-12_Apkpure.apk");
+        capabilities.setCapability("app", "C:\\Users\\USER\\IdeaProjects\\mobileAuto\\apks\\Wikipedia_2.7.50437-r-2023-04-12_Apkpure.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
@@ -31,6 +36,161 @@ public class FirstTest {
 
     @Test
     public void firstTest() {
-        System.out.println("First test run");
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/fragment_onboarding_skip_button']"),
+                "Не найдена кнопка пропуска настроек",
+                5);
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Не открывается главная страница приложения",
+                5);
+
+        waitForElementAndSendKeys(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_src_text']"),
+                "java",
+                "Не найдена строка поиска",
+                5);
+
+        waitForElementPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']//*[@text='Object-oriented programming language']"),
+                "Не отображается результат поиска",
+                15);
     }
+
+    @Test
+    public void testCancelSearch() {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
+                "Не найдена кнопка пропуска настроек",
+                5);
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Не найдена строка поиска",
+                5);
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                " ",
+                "Не удалось ввести значение в строку поиска",
+                5);
+
+        waitForElementAndClear(By.id("org.wikipedia:id/search_src_text"),
+                "Не удалось очистить строку поиска",
+                5);
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                " ",
+                "Не удалось ввести значение в строку поиска",
+                5);
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Не найдена кнопка X отмены поиска",
+                5);
+
+        waitForElementNotPresent(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Не исчезла кнопка X отмены поиска",
+                5);
+    }
+
+    @Test
+    public void testCompareArticleTitle() {
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/fragment_onboarding_skip_button"),
+                "Не найдена кнопка пропуска настроек",
+                5);
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Не найдена строка поиска",
+                5);
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "java",
+                "Не удалось ввести значение в строку поиска",
+                5);
+
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']//*[@text='Object-oriented programming language']"),
+                "Не отображается результат поиска",
+                15);
+
+        WebElement titleElement = waitForElementPresent(
+                By.id("pcs-edit-section-title-description"),
+                "Не удалось найти заголовок статьи",
+                15);
+
+        String articleTitle = titleElement.getAttribute("content-desc");
+
+        Assert.assertEquals(
+                "Заловок статьи не соответсвует ожидаемому результату",
+                "Object-oriented programming language",
+                articleTitle
+        );
+    }
+
+    @Test
+    public void checkWikiSearchTextTest() {
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/fragment_onboarding_skip_button']"),
+                "Не найдена кнопка пропуска настроек",
+                5);
+
+        assertElementHasText(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_container']//*[@class='android.widget.TextView']"),
+                "Search Wikipedia",
+                "Элемент не содержит ожидаемого текста");
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message) {
+        return waitForElementPresent(by, error_message, 5);
+    }
+
+    private WebElement waitForElementAndClick(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private boolean waitForElementNotPresent(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+
+    private WebElement waitForElementAndClear(By by, String errorMessage, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, errorMessage, timeoutInSeconds);
+        element.clear();
+        return element;
+    }
+
+    private WebElement assertElementHasText(By by, String expectedText, String errorMessage) {
+        WebElement element = waitForElementPresent(by, "Не найден элемент", 5);
+        String actualText = element.getText();
+        Assert.assertEquals(
+                errorMessage,
+                expectedText,
+                actualText
+        );
+        return element;
+    }
+
 }
